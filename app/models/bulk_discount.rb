2d232b9a1d732cfaa1a9140253c2a26653b58747
Bryce Simonds <bsimonds14@gmail.com>
2022-08-05 10:49:41 -0600

    Feat: bulk discount belongs to a merchant relationship set up

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,9 +1,6 @@
 class BulkDiscount < ApplicationRecord
     validates :percent_discount, numericality: { only_integer: true }
     validates :quantity_threshold, numericality: { only_integer: true }
-  
-    # has_many :invoices
-    # has_many :invoice_items, through: :invoices
-    # has_many :items, through: :invoice_items
-    # has_many :merchants, through: :items
+
+    belongs_to :merchant
   end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe BulkDiscount do
   end
 
   describe 'relationships' do
-    it { should belong_to :merchants }
+    it { should belong_to(:merchant) }
   end
 end
 
