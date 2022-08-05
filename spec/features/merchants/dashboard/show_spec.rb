68433224ed73a13adb54f8d3086241b0addd09b1
Bryce Simonds <bsimonds14@gmail.com>
2022-08-05 11:40:15 -0600

    Test: has link that brings you to that merchants discounts page

--- spec/features/merchants/dashboard/show_spec.rb
+++ spec/features/merchants/dashboard/show_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
+require 'faker'
 
-RSpec.describe 'Merchant Show Dashboard' do
+RSpec.describe 'Merchant Dashboard' do
     it 'has the name of the merchant' do
         merchant_1 = Merchant.create!(name: 'Spongebob The Merchant')
         merchant_2 = Merchant.create!(name: 'Jon Doe')
@@ -326,5 +327,41 @@ RSpec.describe 'Merchant Show Dashboard' do
           expect(page).to have_content("Monday, January 1, 2001")
           expect(page).to_not have_content(invoice_1.created_at)
         end
+    end
+
+    it 'has a link to all their discounts' do
+        merchant_1 = Merchant.create!(name: Faker::Name.name)
+
+        item_1 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 500, merchant_id: merchant_1.id )
+        item_2 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 500, merchant_id: merchant_1.id )
+        item_3 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 500, merchant_id: merchant_1.id )
+        item_4 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 500, merchant_id: merchant_1.id )
+        item_5 = Item.create!(name: Faker::Beer.name, description: Faker::Beer.style, unit_price: 500, merchant_id: merchant_1.id )
+
+        customer_1 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+        customer_2 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+        customer_3 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+        customer_4 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+
+        invoice_1 = Invoice.create!(status: 0, created_at: Time.new(2000), customer_id: customer_1.id)
+        invoice_2 = Invoice.create!(status: 1, created_at: Time.new(2001), customer_id: customer_2.id)
+        invoice_3 = Invoice.create!(status: 1, created_at: Time.new(2002), customer_id: customer_3.id)
+        invoice_4 = Invoice.create!(status: 2, created_at: Time.new(2003), customer_id: customer_4.id)
+
+        invoice_item_1 = InvoiceItem.create!(quantity: 4, unit_price: 800, status: 1, item_id: item_1.id, invoice_id: invoice_1.id)
+        invoice_item_2 = InvoiceItem.create!(quantity: 2, unit_price: 1400, status: 1, item_id: item_2.id, invoice_id: invoice_1.id)
+        invoice_item_3 = InvoiceItem.create!(quantity: 3, unit_price: 666, status: 2, item_id: item_3.id, invoice_id: invoice_1.id)
+        invoice_item_4 = InvoiceItem.create!(quantity: 2, unit_price: 1400, status: 1, item_id: item_4.id, invoice_id: invoice_2.id)
+        invoice_item_5 = InvoiceItem.create!(quantity: 3, unit_price: 666, status: 2, item_id: item_5.id, invoice_id: invoice_3.id)
+        invoice_item_6 = InvoiceItem.create!(quantity: 3, unit_price: 666, status: 2, item_id: item_5.id, invoice_id: invoice_3.id)
+
+        visit "/merchants/#{merchant_1.id}/dashboard"
+
+        within '#nav' do
+            expect(page).to have_link("View All My Discounts")
+            click_link 'View All My Discounts'
+        end
+
+        expect(current_path).to eq("/merchants/#{merchant_1.id}/bulk_discounts")
     end
 end
