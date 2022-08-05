97437743a7b3c64fd9bbc9f0ccb0af060ab45b7a
Bryce Simonds <bsimonds14@gmail.com>
2022-08-05 13:17:32 -0600

    Test: page has each associated discount along with its attributes

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,5 @@
 class BulkDiscount < ApplicationRecord
-    validates :percent_discount, numericality: { only_integer: true }
+    validates :percent_discount, numericality: { less_than_or_equal_to: 100, only_integer: true }
     validates :quantity_threshold, numericality: { only_integer: true }
 
     belongs_to :merchant
