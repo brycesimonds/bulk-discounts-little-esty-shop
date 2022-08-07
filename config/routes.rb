3b0690119d8012d78f1aa8bc992a0f5aecd796ed
Bryce Simonds <bsimonds14@gmail.com>
2022-08-07 15:53:48 -0600

    Added delete route

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     get '/merchants/:merchant_id/bulk_discounts/new', to: 'merchant_bulk_discounts#new'
     get '/merchants/:merchant_id/bulk_discounts/:bulk_discount_id', to: 'merchant_bulk_discounts#show'
     post '/merchants/:merchant_id/bulk_discounts', to: 'merchant_bulk_discounts#create'
+    delete '/merchants/:merchant_id/bulk_discounts', to: 'merchant_bulk_discounts#destory'
+
     
 
   resources :admin, only: :index
