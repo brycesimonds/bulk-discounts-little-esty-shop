7702785e101d5e64517d8e06303681a4b170b22d
Bryce Simonds <bsimonds14@gmail.com>
2022-08-07 14:36:00 -0600

    Feat: can create a bulk discount when entering valid data

--- app/controllers/merchant_bulk_discounts_controller.rb
+++ app/controllers/merchant_bulk_discounts_controller.rb
@@ -10,4 +10,23 @@ class MerchantBulkDiscountsController < ApplicationController
     def new
         @merchant = Merchant.find(params[:merchant_id])
     end
+
+    def create
+        merchant = Merchant.find(params[:merchant_id])
+
+        merchant_discount = BulkDiscount.new(merchant_bulk_discount_params)
+    
+        if merchant_discount.save
+          redirect_to "/merchants/#{merchant.id}/bulk_discounts"
+        else
+          redirect_to "/merchants/#{merchant.id}/bulk_discounts/new"
+          flash[:alert] = "Error: #{error_message(merchant_discount.errors)}"
+        end 
+    end
+
+    private
+
+    def merchant_bulk_discount_params
+      params.permit(:percent_discount, :quantity_threshold, :merchant_id)
+    end
 end
