df5abc318f08e5c1bd14d53428dd5ca5b47a706e
Bryce Simonds <bsimonds14@gmail.com>
2022-08-09 16:18:46 -0600

    Refactor: added test to achieve 100% coverage in models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'faker'
 
 RSpec.describe Customer do
   describe 'validations' do
@@ -11,5 +12,78 @@ RSpec.describe Customer do
     it { should have_many(:invoice_items).through(:invoices)}
     it { should have_many(:items).through(:invoice_items)}
     it { should have_many(:merchants).through(:items)}
+  end
+
+  describe 'class methods' do
+    it 'calculates the top 5 customers' do
+      merchant_1 = Merchant.create!(name: Faker::Name.name)
+      merchant_2 = Merchant.create!(name: Faker::Name.name)
+      merchant_3 = Merchant.create!(name: Faker::Name.name)
+      merchant_4 = Merchant.create!(name: Faker::Name.name)
+      merchant_5 = Merchant.create!(name: Faker::Name.name)
+      merchant_6 = Merchant.create!(name: Faker::Name.name)
+
+      customer_1 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+      customer_2 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+      customer_3 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+      customer_4 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+      customer_5 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+      customer_6 = Customer.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+
+      item_1 = Item.create!(name: 'Spatula', description: 'It is for cooking', unit_price: 3, merchant_id: merchant_1.id)
+      item_2 = Item.create!(name: 'Spoon', description: 'It is for eating ice cream', unit_price: 1, merchant_id: merchant_1.id)
+      item_3 = Item.create!(name: 'Knife', description: 'It is for slicing bread', unit_price: 5, merchant_id: merchant_1.id)
+      item_4 = Item.create!(name: 'Computer', description: 'It is for playing games', unit_price: 50, merchant_id: merchant_1.id)
+      item_5 = Item.create!(name: 'Table', description: 'It is for eating at', unit_price: 70, merchant_id: merchant_1.id)
+      item_5 = Item.create!(name: 'Bag of Money', description: 'It is for whatever you want', unit_price: 999, merchant_id: merchant_1.id)
+      item_6 = Item.create!(name: 'Decorative Wood 7', description: 'It is a piece of wood shaped like a 7', unit_price: 400, merchant_id: merchant_1.id)
+      item_7 = Item.create!(name: 'Bag of Jacks', description: 'It is for playing Jacks', unit_price: 3, merchant_id: merchant_2.id)
+      item_8 = Item.create!(name: 'Spatula2', description: 'It is for cooking', unit_price: 3, merchant_id: merchant_2.id)
+      item_9 = Item.create!(name: 'Spoon2', description: 'It is for eating ice cream', unit_price: 1, merchant_id: merchant_3.id)
+      item_10 = Item.create!(name: 'Knife2', description: 'It is for slicing bread', unit_price: 5, merchant_id: merchant_4.id)
+      item_11 = Item.create!(name: 'Computer2', description: 'It is for playing games', unit_price: 50, merchant_id: merchant_5.id)
+      item_12 = Item.create!(name: 'Table2', description: 'It is for eating at', unit_price: 70, merchant_id: merchant_6.id)
+      item_13 = Item.create!(name: 'Bag of Money2', description: 'It is for whatever you want', unit_price: 999, merchant_id: merchant_6.id)
+      item_14 = Item.create!(name: 'Decorative Wood 72', description: 'It is a piece of wood shaped like a 7', unit_price: 400, merchant_id: merchant_6.id)
+
+      invoice_1 = Invoice.create!(customer_id: customer_1.id, status: 2)
+      invoice_2 = Invoice.create!(customer_id: customer_2.id, status: 2)
+      invoice_3 = Invoice.create!(customer_id: customer_3.id, status: 2)
+      invoice_4 = Invoice.create!(customer_id: customer_4.id, status: 2)
+      invoice_5 = Invoice.create!(customer_id: customer_5.id, status: 2)
+      invoice_6 = Invoice.create!(customer_id: customer_1.id, status: 2)
+      invoice_7 = Invoice.create!(customer_id: customer_2.id, status: 2)
+      invoice_8 = Invoice.create!(customer_id: customer_3.id, status: 1)
+
+      transaction_1 = Transaction.create!(invoice_id: invoice_1.id, credit_card_number: '983475', credit_card_expiration_date: nil, result: 'success')
+      transaction_2 = Transaction.create!(invoice_id: invoice_2.id, credit_card_number: '345', credit_card_expiration_date: nil, result: 'success')
+      transaction_3 = Transaction.create!(invoice_id: invoice_3.id, credit_card_number: '34657865', credit_card_expiration_date: nil, result: 'success')
+      transaction_4 = Transaction.create!(invoice_id: invoice_4.id, credit_card_number: '3456546', credit_card_expiration_date: nil, result: 'success')
+      transaction_5 = Transaction.create!(invoice_id: invoice_5.id, credit_card_number: '234234', credit_card_expiration_date: nil, result: 'success')
+      transaction_6 = Transaction.create!(invoice_id: invoice_6.id, credit_card_number: '6578', credit_card_expiration_date: nil, result: 'success')
+      transaction_7 = Transaction.create!(invoice_id: invoice_7.id, credit_card_number: '9789', credit_card_expiration_date: nil, result: 'success')
+      transaction_8 = Transaction.create!(invoice_id: invoice_8.id, credit_card_number: '3456', credit_card_expiration_date: nil, result: 'failed')
+
+      invoice_item_1 = InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 5, unit_price: 2, status: 2)
+      invoice_item_2 = InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_1.id, quantity: 3, unit_price: 3, status: 2)
+      invoice_item_3 = InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_2.id, quantity: 2, unit_price: 1, status: 2)
+      invoice_item_4 = InvoiceItem.create!(item_id: item_4.id, invoice_id: invoice_2.id, quantity: 4, unit_price: 2, status: 2)
+      invoice_item_5 = InvoiceItem.create!(item_id: item_5.id, invoice_id: invoice_3.id, quantity: 3, unit_price: 3, status: 2)
+      invoice_item_7 = InvoiceItem.create!(item_id: item_6.id, invoice_id: invoice_3.id, quantity: 4, unit_price: 1, status: 2)
+      invoice_item_8 = InvoiceItem.create!(item_id: item_7.id, invoice_id: invoice_4.id, quantity: 4, unit_price: 1, status: 2)
+      invoice_item_9 = InvoiceItem.create!(item_id: item_8.id, invoice_id: invoice_4.id, quantity: 4, unit_price: 1, status: 2)
+      invoice_item_10 = InvoiceItem.create!(item_id: item_9.id, invoice_id: invoice_5.id, quantity: 3, unit_price: 4, status: 2)
+      invoice_item_11 = InvoiceItem.create!(item_id: item_10.id, invoice_id: invoice_6.id, quantity: 4, unit_price: 1, status: 2)
+      invoice_item_12 = InvoiceItem.create!(item_id: item_11.id, invoice_id: invoice_7.id, quantity: 2, unit_price: 3, status: 2)
+      invoice_item_13 = InvoiceItem.create!(item_id: item_12.id, invoice_id: invoice_8.id, quantity: 4, unit_price: 1, status: 2)
+      invoice_item_13 = InvoiceItem.create!(item_id: item_13.id, invoice_id: invoice_8.id, quantity: 4, unit_price: 1, status: 2)
+      invoice_item_13 = InvoiceItem.create!(item_id: item_14.id, invoice_id: invoice_8.id, quantity: 4, unit_price: 1, status: 2)
+      
+      expect(Customer.top_five_customers.first).to eq(customer_1)
+      expect(Customer.top_five_customers[1]).to eq(customer_2)
+      expect(Customer.top_five_customers[2]).to eq(customer_3)
+      expect(Customer.top_five_customers[3]).to eq(customer_4)
+      expect(Customer.top_five_customers.last).to eq(customer_5)
+    end
   end
 end
